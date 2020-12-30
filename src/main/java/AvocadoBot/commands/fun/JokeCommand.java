01a7b71769f5c7093c1d30ac6f2e2be753ec3ed0
Yash V <yvvijay121@gmail.com>
2020-12-30 12:54:37 -0600

    added UserAgent to JokeCommand.java, changed .gitignore slightly

--- src/main/java/AvocadoBot/commands/fun/JokeCommand.java
+++ src/main/java/AvocadoBot/commands/fun/JokeCommand.java
@@ -1,20 +1,6 @@
 package AvocadoBot.commands.fun;
 
 import AvocadoBot.Main;
-import java.awt.Color;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
@@ -22,23 +8,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.awt.*;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class JokeCommand implements MessageCreateListener {
 
     private static String prefix;
 
     public JokeCommand(String pre) {
         prefix = pre;
-    }
-
-    private static String readAll(InputStream is) throws IOException {
-        Scanner sc = new Scanner(is);
-        //StringBuffer to store the contents
-        StringBuilder sb = new StringBuilder();
-        //Appending each line to the buffer
-        while (sc.hasNext()) {
-            sb.append(" ").append(sc.nextLine());
-        }
-        return sb.toString();
     }
 
     private static String readAll(File f) throws IOException {
@@ -52,18 +37,21 @@ public class JokeCommand implements MessageCreateListener {
         return sb.toString();
     }
 
-    private static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+    private static JSONObject readJsonFromUrl(URLConnection url) throws IOException, JSONException {
+        JSONObject json = null;
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(url.getInputStream()));
             StringBuilder sb = new StringBuilder();
             int cp;
             while ((cp = rd.read()) != -1) {
                 sb.append((char) cp);
             }
             String jsonText = sb.toString();
-            JSONObject json = new JSONObject(jsonText);
-            return json;
+            json = new JSONObject(jsonText);
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
         }
+        return json;
     }
 
     @Override
@@ -71,22 +59,23 @@ public class JokeCommand implements MessageCreateListener {
         if (event.getMessageContent().equalsIgnoreCase(prefix + "joke")) {
             JSONObject j1 = new JSONObject();
             try {
-                j1 = readJsonFromUrl("https://www.reddit.com/r/jokes.json?limit=100");
-                URI urlos = Main.class.getResource("/jokes.json").toURI();
-                File f1 = new File(urlos);
+                URL url = new URL("https://www.reddit.com/r/jokes.json?limit=100");
+                URLConnection hc = url.openConnection();
+                hc.setRequestProperty("User-Agent", "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36");
+                j1 = readJsonFromUrl(hc);
+                File f1 = new File("C:\\Programming_Projects\\ResourcesForMigratableProjects\\jokes.json");
                 try (FileWriter fw1 = new FileWriter(f1)) {
                     fw1.write(j1.toString(3));
                 }
-            } catch (IOException | JSONException | URISyntaxException ex) {
+            } catch (IOException | JSONException ex) {
                 Logger.getLogger(JokeCommand.class.getName()).log(Level.WARNING, null, ex);
             }
             if (j1.isEmpty()) {
                 try {
-                    URI urlos = Main.class.getResource("/jokes.json").toURI();
-                    File f2 = new File(urlos);
+                    File f2 = new File("C:\\Programming_Projects\\ResourcesForMigratableProjects\\jokes.json");
                     j1 = new JSONObject(readAll(f2));
                     System.out.println("everything went fine (old read)");
-                } catch (URISyntaxException | IOException ex) {
+                } catch (IOException ex) {
                     Logger.getLogger(JokeCommand.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
@@ -119,7 +108,7 @@ public class JokeCommand implements MessageCreateListener {
                             .setColor(new Color(204, 44, 44));
                     event.getChannel().sendMessage(embed);
                 }
-            }else{
+            } else {
                 event.getChannel().sendMessage("something went wrong.");
             }
         }
