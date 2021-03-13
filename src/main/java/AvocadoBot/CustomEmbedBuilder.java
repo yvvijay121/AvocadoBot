d689e6d435ad23ce68809c0c43038ffb5c427863
Yv V <yvvijay121@gmail.com>
2021-03-13 10:09:42 -0600

    I implemented a Discord command framework in most of the commands in the bot, I created a new help command based on this command framework, and I made a CustomEmbedBuilder class that I plan to implement in all of my commands in the future.

---
+++ src/main/java/AvocadoBot/CustomEmbedBuilder.java
@@ -0,0 +1,2 @@
+package AvocadoBot;public class CustomEmbedBuilder {
+}
--- src/main/java/AvocadoBot/commands/HelpCommand.java
+++ src/main/java/AvocadoBot/commands/HelpCommand.java
@@ -1,40 +1,30 @@
 package AvocadoBot.commands;
 
-import java.awt.Color;
+import AvocadoBot.CustomEmbedBuilder;
+import AvocadoBot.Main;
+import de.btobastian.sdcf4j.Command;
+import de.btobastian.sdcf4j.CommandExecutor;
+import de.btobastian.sdcf4j.CommandHandler;
+import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.event.message.MessageCreateEvent;
-import org.javacord.api.exception.MissingPermissionsException;
-import org.javacord.api.listener.message.MessageCreateListener;
-import org.javacord.api.util.logging.ExceptionLogger;
 
-public class HelpCommand implements MessageCreateListener {
-    private static String prefix;
-    
-    public HelpCommand(String pre){
-        prefix = pre;
-    }
-    
-    @Override
-    public void onMessageCreate(MessageCreateEvent event) {
-        if (event.getMessageContent().equalsIgnoreCase(prefix + "help")) {
-            EmbedBuilder embed = new EmbedBuilder()
-                    .setTitle("Help Menu")
-                    .setDescription("This is a work in progress, mention the developer for help.")
-                    .addInlineField("`" + prefix + "ping" + "`", "Testing command.")
-                    .addInlineField("`" + prefix + "kick" + "`", "Kicks the mentioned user.")
-                    .addInlineField("`" + prefix + "ban" + "`", "Bans the mentioned user.")
-                    .addInlineField("`" + prefix + "unban" + "`", "Unbans the mentioned user.")
-                    .addInlineField("`" + prefix + "mute" + "`", "Mutes the mentioned user.")
-                    .addInlineField("`" + prefix + "tempmute" + "`", "Temporarily mutes the mentioned user for a specified number of seconds.")
-                    .addInlineField("`" + prefix + "unmute" + "`", "Unmutes the mentioned user.")
-                    .addInlineField("`" + prefix + "clear" + "`", "Clears the specified number of messages in a text channel.")
-                    .addInlineField("`" + prefix + "userinfo" + "`", "Gets the user info of the mentioned user, or the author if user not specified.")
-                    .addInlineField("`" + prefix + "udict" + "`", "Gets the definition of a specified work from Urban Dictionary, or a random word if no search specified.")
-                    .addInlineField("`" + prefix + "xkcd" + "`", "Gets a random comic from XKCD.")
-                    .addInlineField("`" + prefix + "joke" + "`", "Gets a random joke from r/jokes.")
-                    .setColor(new Color(204,44,44));
-            event.getChannel().sendMessage(embed).exceptionally(ExceptionLogger.get(MissingPermissionsException.class));
-        }
+public class HelpCommand implements CommandExecutor {
+    private final CommandHandler commandHandler;
+
+    public HelpCommand(CommandHandler commandHandler) {
+        this.commandHandler = commandHandler;
     }
 
+    @Command(aliases = {"help", "commands"}, description = "Shows this page.", usage = "help")
+    public void onHelpCommand(TextChannel tc) {
+        EmbedBuilder eb = new CustomEmbedBuilder();
+        eb.setTitle("Help Menu");
+        eb.setDescription("This is a work in progress, mention the developer for help.");
+        commandHandler.getCommands().stream()
+                .map(CommandHandler.SimpleCommand::getCommandAnnotation)
+                .filter(Command::showInHelpPage)
+                .forEach(y -> eb.addInlineField("`" + Main.prefix +
+                        (y.usage().isEmpty() ? y.aliases()[0] : y.usage()) + "`", y.description()));
+        tc.sendMessage(eb);
+    }
 }
